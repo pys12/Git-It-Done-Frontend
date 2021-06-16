acdf7353d1d073925349db23ef504ea43578f2f6
pys12 <peiyishi11@gmail.com>
2021-06-15 22:29:58 -0400

    create route setup to connect with db

--- src/components/Task.js
+++ src/components/Task.js
@@ -3,9 +3,9 @@ import React from 'react'
 const Task = ({task}) => {
     return (
     <div>
-        <div>{task.task}</div>
+        <div>{task.title}</div>
         <div>{task.description}</div>
-        <div>{task.category}</div>
+        <div>{task.categories}</div>
     </div>
     )
 }
--- src/components/TaskContainer.js
+++ src/components/TaskContainer.js
@@ -1,31 +1,58 @@
-import React,{useState} from 'react'
+import React,{useState,useEffect} from 'react'
 import Task from './Task'
 //import { Link } from 'react-router-dom';
 
 const TaskContainer = () => {
-    const [tasks, setTasks] = useState([])
-    const [task, setTask] = useState('')
-    const [description,setDescription] = useState('')
-    const [category, setCategory] = useState('')
+    const [tasks, setTasks] = useState(null)
+    const URL = "http://localhost:5000/api/tasks";
     
-    const onSave = (task) => {
-        const newTask = { ...task }
-        setTasks([...tasks, newTask])
-        console.log(task)
-        console.log(tasks)
-    }
-    const onSubmit = (e) => {
+    const [form, setForm] = useState({
+        title: "",
+        description: "",
+        categories:""
+    })
+   
+    
+    const getTasks = async () => {
+        const response = await fetch(URL);
+        const data = await response.json();
+        setTasks(data);
+    };
+    const createTask = async (task) => {
+        await fetch(URL, {
+          method: "post",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(task)
+        }).then(console.log(task));
+
+        getTasks();
+    };
+    const handleChange = (e) => {
+        console.log(e.target.name);
+        setForm({...form, [e.target.name]: e.target.value });
+        console.log(form)
+      };
+    
+    const handleSubmit = (e) => {
         e.preventDefault()
+        createTask(form);
+        setForm({
+            title: "",
+            description: "",
+            categories:""
+        })
+    }
+    useEffect(() => getTasks(), []);
     
-        if (!task) {
-          alert('Please add a task')
-          return
-        }
-        onSave({ task, description, category })
-        
-        setTask('')
-        setDescription('')
-        setCategory('')
+    const loaded = () => {
+        return tasks.map((task, index) => (
+            <Task key={index} task={task}  />
+        ))
+    }
+    const loading = () => {
+        return <h1>loading now..</h1>
     }
     
     return (
@@ -33,20 +60,19 @@ const TaskContainer = () => {
             <h1>Tasks Area</h1>
                 <div>Create Task</div>
                 {/* add hover effects here */}
-            <form  onSubmit={onSubmit}>
+            <form  onSubmit={handleSubmit}>
                 <div>
-                    <label>Task</label>
-                    <input type="text" placeholder='Add a task' value={task} onChange={(e)=>{setTask(e.target.value)}}/>
+                    <label>Title</label>
+                    <input type="text" name='title' placeholder='Add a task' value={form.title} onChange={handleChange}/>
                     <label>Description</label>
-                    <input type="text" placeholder='Add a detailed description' value={description} onChange={(e)=>{setDescription(e.target.value)}}/>
-                    <label>Category</label>
-                    <input type="text" placeholder='Pick a category' value={category} onChange={(e)=>{setCategory(e.target.value)}}/>
-                    <input type="submit" value='Save' onClick={onSave}/>
+                    <input type="text" name='description' placeholder='Add a detailed description' value={form.description} onChange={handleChange}/>
+                    <label>Categories</label>
+                    <input type="text" name='categories' placeholder='Pick a category' value={form.categories} onChange={handleChange}/>
+                    <input type="submit" value='Save' />
                 </div>
+                {tasks ? loaded() : loading()}
             </form>
-            {tasks.map((task, index) => (
-                <Task key={index} task={task}  />
-             ))}
+            
         </div>
     )
 }
