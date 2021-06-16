b26685846535f80f70b0b7c7e6e41c46e5c4c2a3
handsomevang <legithandsomevang@gmail.com>
2021-06-16 16:23:23 -0400

    fixing merge conflicts

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import React from 'react'
 import Sidebar from './components/Sidebar'
 import Tasks from './components/TaskContainer'
 import Header from './components/Header'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Button, Container } from 'react-bootstrap'
 
 function App() {
   return (
     <div className="App">
+      <Container>
       <Header />
       <Sidebar />
       <Tasks />
+      <Button>Button</Button>
+      </Container>
     </div>
   );
 }
